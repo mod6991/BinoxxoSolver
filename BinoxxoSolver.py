8560ea6ba401b41f885fff7c7d0d8035af4c2991
Josue Clement <mod6991@gmail.com>
2021-02-18 14:41:58 +0100

    First commit

---
+++ BinoxxoSolver.py
@@ -0,0 +1,323 @@
+class Binoxxo:
+    def __init__(self):
+        self.gb = []
+        for row_i in range(10):
+            self.gb.append([])
+            for col_i in range(10):
+                self.gb[row_i].append(None)
+
+    @staticmethod
+    def get_opposite(val):
+        if val == 'X':
+            return 'O'
+        return 'X'
+
+    def get_row(self, row_i):
+        return self.gb[row_i]
+
+    def get_col(self, col_i):
+        col = []
+        for row in self.gb:
+            col.append(row[col_i])
+        return col
+
+    def solve_is_surrounded(self):
+        solved_anything = False
+
+        # horizontal check
+        for row_i in range(10):
+            for col_i in range(1, 9):
+                cell_val = self.gb[row_i][col_i]
+                left_val = self.gb[row_i][col_i - 1]
+                right_val = self.gb[row_i][col_i + 1]
+                if not cell_val and left_val and right_val:
+                    if left_val == right_val:
+                        self.gb[row_i][col_i] = Binoxxo.get_opposite(left_val)
+                        print(f'({row_i}, {col_i}) was surrounded horizontally')
+                        solved_anything = True
+
+        # vertical check
+        for row_i in range(1, 9):
+            for col_i in range(10):
+                cell_val = self.gb[row_i][col_i]
+                left_val = self.gb[row_i - 1][col_i]
+                right_val = self.gb[row_i + 1][col_i]
+                if not cell_val and left_val and right_val:
+                    if left_val == right_val:
+                        self.gb[row_i][col_i] = Binoxxo.get_opposite(left_val)
+                        print(f'({row_i}, {col_i}) was surrounded vertically')
+                        solved_anything = True
+
+        return solved_anything
+
+    def solve_twins(self):
+        solved_anything = False
+
+        # horizontal check
+        for row_i in range(10):
+            for col_i in range(1, 9):
+                cell_val = self.gb[row_i][col_i]
+                left_val = self.gb[row_i][col_i - 1]
+                right_val = self.gb[row_i][col_i + 1]
+
+                if cell_val and left_val and cell_val == left_val and not right_val:
+                    self.gb[row_i][col_i + 1] = Binoxxo.get_opposite(cell_val)
+                    print(f'({row_i}, {col_i + 1}) has twins on the left')
+                    solved_anything = True
+                elif cell_val and right_val and cell_val == right_val and not left_val:
+                    self.gb[row_i][col_i - 1] = Binoxxo.get_opposite(cell_val)
+                    print(f'({row_i}, {col_i - 1}) has twins on the right')
+                    solved_anything = True
+
+        # vertical check
+        for row_i in range(1, 9):
+            for col_i in range(10):
+                cell_val = self.gb[row_i][col_i]
+                top_val = self.gb[row_i - 1][col_i]
+                bot_val = self.gb[row_i + 1][col_i]
+
+                if cell_val and top_val and cell_val == top_val and not bot_val:
+                    self.gb[row_i + 1][col_i] = Binoxxo.get_opposite(cell_val)
+                    print(f'({row_i + 1}, {col_i}) has twins on the top')
+                    solved_anything = True
+                elif cell_val and bot_val and cell_val == bot_val and not top_val:
+                    self.gb[row_i - 1][col_i] = Binoxxo.get_opposite(cell_val)
+                    print(f'({row_i - 1}, {col_i}) has twins on the bottom')
+                    solved_anything = True
+
+        return solved_anything
+
+    @staticmethod
+    def count(lst):
+        count_o, count_x = 0, 0
+        for item in lst:
+            if item == 'O':
+                count_o += 1
+            elif item == 'X':
+                count_x += 1
+        return count_o, count_x
+
+    def solve_full(self):
+        solved_anything = False
+
+        # horizontal check
+        for row_i in range(10):
+            row = self.get_row(row_i)
+            count_o, count_x = Binoxxo.count(row)
+
+            if count_o == 5 and count_x < 5:
+                for col_i in range(10):
+                    if not self.gb[row_i][col_i]:
+                        self.gb[row_i][col_i] = 'X'
+                print(f'fill row {row_i} with X because 5 O were found')
+                solved_anything = True
+
+            if count_x == 5 and count_o < 5:
+                for col_i in range(10):
+                    if not self.gb[row_i][col_i]:
+                        self.gb[row_i][col_i] = 'O'
+                print(f'fill row {row_i} with O because 5 X were found')
+                solved_anything = True
+
+        # vertical check
+        for col_i in range(10):
+            col = self.get_col(col_i)
+            count_o, count_x = Binoxxo.count(col)
+
+            if count_o == 5 and count_x < 5:
+                for row_i in range(10):
+                    if not self.gb[row_i][col_i]:
+                        self.gb[row_i][col_i] = 'X'
+                print(f'fill col {col_i} with X because 5 O were found')
+                solved_anything = True
+
+            if count_x == 5 and count_o < 5:
+                for row_i in range(10):
+                    if not self.gb[row_i][col_i]:
+                        self.gb[row_i][col_i] = 'O'
+                print(f'fill col {col_i} with O because 5 X were found')
+                solved_anything = True
+
+        return solved_anything
+
+    @staticmethod
+    def get_risk(lst, risk_val):
+        risk_lst = []
+        tmp_lst = []
+
+        for i in range(10):
+            item = lst[i]
+            if not item or item == risk_val:
+                if len(tmp_lst) == 3:
+                    risk_lst.append(tmp_lst)
+                    tmp_lst = [i]
+                else:
+                    tmp_lst.append(i)
+            else:
+                if tmp_lst:
+                    risk_lst.append(tmp_lst)
+                    tmp_lst = []
+
+        if tmp_lst:
+            risk_lst.append(tmp_lst)
+
+        return risk_lst
+
+    @staticmethod
+    def is_risky(lst):
+        for sub_lst in lst:
+            if len(sub_lst) == 3:
+                return True
+        return False
+
+    def solve_4lt4(self):
+        solved_anything = False
+
+        # horizontal check
+        for row_i in range(10):
+            row = self.get_row(row_i)
+            count_o, count_x = Binoxxo.count(row)
+
+            if count_o == 4 and count_x < 4:
+                lst = Binoxxo.get_risk(row, 'X')
+                is_risky = Binoxxo.is_risky(lst)
+                if is_risky:
+                    for sub_lst in lst:
+                        if len(sub_lst) < 3:
+                            for col_i in sub_lst:
+                                if not self.gb[row_i][col_i]:
+                                    self.gb[row_i][col_i] = 'X'
+                                    print(f'({row_i}, {col_i}) can only be X on the row')
+                                    solved_anything = True
+
+            elif count_x == 4 and count_o < 4:
+                lst = Binoxxo.get_risk(row, 'O')
+                is_risky = Binoxxo.is_risky(lst)
+                if is_risky:
+                    for sub_lst in lst:
+                        if len(sub_lst) < 3:
+                            for col_i in sub_lst:
+                                if not self.gb[row_i][col_i]:
+                                    self.gb[row_i][col_i] = 'O'
+                                    print(f'({row_i}, {col_i}) can only be O on the row')
+                                    solved_anything = True
+
+        # vertical check
+        for col_i in range(10):
+            col = self.get_col(col_i)
+            count_o, count_x = Binoxxo.count(col)
+
+            if count_o == 4 and count_x < 4:
+                lst = Binoxxo.get_risk(col, 'X')
+                is_risky = Binoxxo.is_risky(lst)
+                if is_risky:
+                    for sub_lst in lst:
+                        if len(sub_lst) < 3:
+                            for row_i in sub_lst:
+                                if not self.gb[row_i][col_i]:
+                                    self.gb[row_i][col_i] = 'X'
+                                    print(f'({row_i}, {col_i}) can only be X on the col')
+                                    solved_anything = True
+
+            elif count_x == 4 and count_o < 4:
+                lst = Binoxxo.get_risk(col, 'O')
+                is_risky = Binoxxo.is_risky(lst)
+                if is_risky:
+                    for sub_lst in lst:
+                        if len(sub_lst) < 3:
+                            for row_i in sub_lst:
+                                if not self.gb[row_i][col_i]:
+                                    self.gb[row_i][col_i] = 'O'
+                                    print(f'({row_i}, {col_i}) can only be O on the col')
+                                    solved_anything = True
+
+        return solved_anything
+
+    @staticmethod
+    def get_empty(lst):
+        lst_empty = []
+
+        for i in range(10):
+            if not lst[i]:
+                lst_empty.append(i)
+
+        return lst_empty
+
+    def check_duplicates_row(self, given_row_i, given_row, lst_empty):
+        solved_anything = False
+
+        for row_i in range(10):
+            if row_i == given_row_i:
+                continue
+
+            row = self.get_row(row_i)
+            count_o, count_x = self.count(row)
+
+            if count_o == 5 and count_x == 5:
+                same = True
+                for i in range(10):
+                    if i in lst_empty:
+                        continue
+                    if row[i] != given_row[i]:
+                        same = False
+
+                if same:
+                    a, b = lst_empty
+                    self.gb[given_row_i][a] = self.gb[row_i][b]
+                    self.gb[given_row_i][b] = self.gb[row_i][a]
+                    print(f'completed row {given_row_i} by swapping with full row {row_i}')
+
+        return solved_anything
+
+    def check_duplicates_col(self, given_col_i, given_col, lst_empty):
+        solved_anything = False
+
+        for col_i in range(10):
+            if col_i == given_col_i:
+                continue
+
+            col = self.get_col(col_i)
+            count_o, count_x = self.count(col)
+
+            if count_o == 5 and count_x == 5:
+                same = True
+                for i in range(10):
+                    if i in lst_empty:
+                        continue
+                    if col[i] != given_col[i]:
+                        same = False
+
+                if same:
+                    a, b = lst_empty
+                    self.gb[a][given_col_i] = self.gb[b][col_i]
+                    self.gb[b][given_col_i] = self.gb[a][col_i]
+                    print(f'completed col {given_col_i} by swapping with full col {col_i}')
+
+        return solved_anything
+
+    def solve_no_duplicates(self):
+        solved_anything = False
+
+        # horizontal check
+        for row_i in range(10):
+            row = self.get_row(row_i)
+            count_o, count_x = Binoxxo.count(row)
+
+            if count_o == 4 and count_x == 4:
+                lst_empty = self.get_empty(row)
+                duplicate_row = self.check_duplicates_row(row_i, row, lst_empty)
+                if duplicate_row:
+                    solved_anything = True
+
+        # vertical check
+        for col_i in range(10):
+            col = self.get_col(col_i)
+            count_o, count_x = Binoxxo.count(col)
+
+            if count_o == 4 and count_x == 4:
+                lst_empty = self.get_empty(col)
+                duplicate_col = self.check_duplicates_col(col_i, col, lst_empty)
+                if duplicate_col:
+                    solved_anything = True
+
+        return solved_anything
---
+++ main.py
@@ -0,0 +1,163 @@
+"""
+Binoxxo solver
+Josué Clément (2021)
+"""
+
+import arcade
+from BinoxxoSolver import Binoxxo
+
+BORDER_WIDTH = 1
+CELL_SIZE = 50
+ROWS, COLS = 10, 10
+SCREEN_WIDTH = (11 * BORDER_WIDTH) + (10 * CELL_SIZE)
+SCREEN_HEIGHT = (11 * BORDER_WIDTH) + (10 * CELL_SIZE)
+SCREEN_TITLE = "Binoxxo solver"
+
+LINES_POSITIONS = \
+[
+    BORDER_WIDTH // 2,
+    (1 * BORDER_WIDTH) + (1 * CELL_SIZE) + (BORDER_WIDTH // 2),
+    (2 * BORDER_WIDTH) + (2 * CELL_SIZE) + (BORDER_WIDTH // 2),
+    (3 * BORDER_WIDTH) + (3 * CELL_SIZE) + (BORDER_WIDTH // 2),
+    (4 * BORDER_WIDTH) + (4 * CELL_SIZE) + (BORDER_WIDTH // 2),
+    (5 * BORDER_WIDTH) + (5 * CELL_SIZE) + (BORDER_WIDTH // 2),
+    (6 * BORDER_WIDTH) + (6 * CELL_SIZE) + (BORDER_WIDTH // 2),
+    (7 * BORDER_WIDTH) + (7 * CELL_SIZE) + (BORDER_WIDTH // 2),
+    (8 * BORDER_WIDTH) + (8 * CELL_SIZE) + (BORDER_WIDTH // 2),
+    (9 * BORDER_WIDTH) + (9 * CELL_SIZE) + (BORDER_WIDTH // 2),
+    (10 * BORDER_WIDTH) + (10 * CELL_SIZE) + (BORDER_WIDTH // 2)
+]
+
+CELLS_START_POSITIONS = \
+[
+    (1 * BORDER_WIDTH),
+    (2 * BORDER_WIDTH) + (1 * CELL_SIZE),
+    (3 * BORDER_WIDTH) + (2 * CELL_SIZE),
+    (4 * BORDER_WIDTH) + (3 * CELL_SIZE),
+    (5 * BORDER_WIDTH) + (4 * CELL_SIZE),
+    (6 * BORDER_WIDTH) + (5 * CELL_SIZE),
+    (7 * BORDER_WIDTH) + (6 * CELL_SIZE),
+    (8 * BORDER_WIDTH) + (7 * CELL_SIZE),
+    (9 * BORDER_WIDTH) + (8 * CELL_SIZE),
+    (10 * BORDER_WIDTH) + (9 * CELL_SIZE)
+]
+
+
+def change_coords(x, y):
+    return x, SCREEN_HEIGHT - y
+
+
+class MyGame(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        arcade.set_background_color(arcade.color.WHITE)
+
+        self.binoxxo = None
+        self.mouse_x = 0
+        self.mouse_y = 0
+        self.cell_row = 0
+        self.cell_col = 0
+
+    def setup(self):
+        """Set up the game variables. Call to re-start the game."""
+        self.binoxxo = Binoxxo()
+        self.mouse_x = 0
+        self.mouse_y = 0
+        self.cell_row = 0
+        self.cell_col = 0
+
+    def on_draw(self):
+        """Render the screen."""
+        # This command should happen before we start drawing. It will clear
+        # the screen to the background color, and erase what we drew last frame.
+        arcade.start_render()
+        self.draw_gameboard()
+
+    def on_key_press(self, key, key_modifiers):
+        """
+        Called whenever a key on the keyboard is pressed.
+        """
+        val = None
+        update_val = False
+        if key == arcade.key.X:
+            val = 'X'
+            update_val = True
+        elif key == arcade.key.O:
+            val = 'O'
+            update_val = True
+        elif key == arcade.key.DELETE:
+            val = None
+            update_val = True
+        elif key in (arcade.key.SPACE, arcade.key.ENTER):
+            self.try_to_solve()
+
+        if update_val:
+            self.binoxxo.gb[self.cell_row][self.cell_col] = val
+
+    def on_mouse_motion(self, x, y, delta_x, delta_y):
+        """
+        Called whenever the mouse moves.
+        """
+        self.mouse_x, self.mouse_y = change_coords(x, y)
+        self.cell_row = self.get_cell(self.mouse_y)
+        self.cell_col = self.get_cell(self.mouse_x)
+
+    def _draw_horizontal_lines(self):
+        for i in range(11):
+            sx, sy = change_coords(0, LINES_POSITIONS[i])
+            ex, ey = change_coords(SCREEN_HEIGHT, LINES_POSITIONS[i])
+            arcade.draw_line(sx, sy, ex, ey, arcade.color.BLACK, BORDER_WIDTH)
+
+    def _draw_vertical_lines(self):
+        for i in range(11):
+            sx, sy = change_coords(LINES_POSITIONS[i], 0)
+            ex, ey = change_coords(LINES_POSITIONS[i], SCREEN_HEIGHT)
+            arcade.draw_line(sx, sy, ex, ey, arcade.color.BLACK, BORDER_WIDTH)
+
+    def draw_gameboard(self):
+        self._draw_horizontal_lines()
+        self._draw_vertical_lines()
+
+        for row_i in range(10):
+            for col_i in range(10):
+                tx, ty = change_coords(CELLS_START_POSITIONS[col_i] + (CELL_SIZE // 2),
+                                       CELLS_START_POSITIONS[row_i] + (CELL_SIZE // 2))
+                arcade.draw_text(self.get_cell_str(row_i, col_i), tx, ty, arcade.color.BLACK, 32,
+                                 width=40, align='center', anchor_x='center', anchor_y='center')
+
+    def get_cell_str(self, row_i, col_i):
+        cell_value = self.binoxxo.gb[row_i][col_i]
+        if not cell_value:
+            return ' '
+        return cell_value
+
+    def get_cell(self, pos):
+        """Get the current cell from mouse position."""
+        for i in range(1, 10):
+            if pos < CELLS_START_POSITIONS[i]:
+                return i - 1
+        return 9
+
+    def try_to_solve(self):
+        while True:
+            s1 = self.binoxxo.solve_is_surrounded()
+            s2 = self.binoxxo.solve_twins()
+            s3 = self.binoxxo.solve_full()
+            s4 = self.binoxxo.solve_4lt4()
+            s5 = self.binoxxo.solve_no_duplicates()
+
+            if not s1 and not s2 and not s3 and not s4 and not s5:
+                print('solve ended')
+                break
+
+
+def main():
+    """Main method"""
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
